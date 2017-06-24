a82ff440ce353b2ea4f2fd9d6c00889bd51a2229
Max Kusnadi <max.kusnadi@gmail.com>
2017-06-24 15:38:32 +0800

    Create a pool

--- src/constants.py
+++ src/constants.py
@@ -9,16 +9,12 @@ PLAYERS = {
     "MaxKusnad": "MaxKusnad-1427",
     "Sanctumsacre": "Sanctumsacre-1927",
     "Stikman": "StikMan-11838",
-    "SW1039": "sw1039-1426",
-    # "Seagull": "Seagull",
+    "Shirousagi": "shirousagi-11769",
     "TitaniumBro": "titaniumBro-1595",
     "Blindtrek": "BlindTrek-1514",
     "Bebekterbang": "bebekterbang-1271",
     "Dinarys": "Dinarys-11713",
-    "WalkingLemon": "WalkingLemon-1490",
     "Vortex": "Vortex-1240"
-    # "Miro": "Miro-31858",
-    # "Rubikon": "Rubikon"
 }
 
 HEROES = {
